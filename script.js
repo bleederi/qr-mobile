614cb5059e8416cbbcb5419195451ff150b66eb3
jeeessseee <bleederi@windowslive.com>
2018-02-14 17:07:53 +0200

    gUM v1

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+constraints = { audio: true, video: { facingMode: "user" } };
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(stream) {
+  /* Usage here */
+})
+.catch(function(err) {
+    console.log('Error!', e);
+});
